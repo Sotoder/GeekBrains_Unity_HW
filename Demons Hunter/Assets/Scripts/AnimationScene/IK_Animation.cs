3d0dbba22ac9e177624f152529a16df27cb85f73
Алексей Скворцов <sotoder@gmail.com>
2021-07-27 17:53:54 +0300

    Выполнены все задания ДЗ 5, кроме задания со звездочкой.

--- Demons Hunter/Assets/Scripts/AnimationScene/IK_Animation.cs
+++ Demons Hunter/Assets/Scripts/AnimationScene/IK_Animation.cs
@@ -1,18 +1,124 @@
-using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class IK_Animation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private bool _isActive;
+    [SerializeField] private Transform _catchObject;
+    [SerializeField] private Transform _catchPoint;
+
+    private Transform _lookObject;
+    private Animator _animator;
+    private float _speed = 0.5f;
+    private float _weight;
+    private Dictionary<Transform, float> _lookObjectContainer = new Dictionary<Transform, float>();
+    private bool _isInVision;
+
+    private void Awake()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        IKActivate(); //ѕо хорошему разгрузить апдейт и убрать метод из него, флаг активации поднимать и опускать по триггеру,
+                      //а вес считать только если игрок остановилс€ в OnAnimatorIK. “огда не будет лишних просчетов, которые никак не используютс€.
+        if (_lookObjectContainer.Count > 0) _lookObject = GetObject(_lookObjectContainer);
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (!_isActive) return;
         
+        if(_lookObject != null)
+        {
+            _animator.SetLookAtWeight(_weight);
+            _animator.SetLookAtPosition(_lookObject.position);
+        }
+
+        if(IsObjectNear(_catchObject) && _isInVision)
+        {
+            //float angle = Vector3.Angle(_catchPoint.position, transform.forward); // ’отел ограничивать углы, но пон€л что смотреть на угол между объектом
+            //Debug.Log(angle);                                                     // и игроком плоха€ зате€. Ќужно смотреть на углы поворота костей модели и
+                                                                                    // ограничивать именно их.
+            
+            _animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, _weight);
+            _animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, _weight);
+            _animator.SetIKPosition(AvatarIKGoal.LeftHand, _catchPoint.position);
+            _animator.SetIKRotation(AvatarIKGoal.LeftHand, _catchPoint.rotation);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("InteractiveLook"))
+        {
+            _lookObjectContainer.Add(other.transform, Vector3.Distance(other.transform.position, transform.position));
+        }
+
+        if (other.CompareTag("InteractiveCatch"))
+        {
+            _isInVision = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("InteractiveLook"))
+        {
+            UpdateDistanceValue(other);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("InteractiveLook"))
+        {
+            _lookObjectContainer.Remove(other.transform);
+            if (_lookObjectContainer.Count < 1) _lookObject = null;
+        }
+
+        if (other.CompareTag("InteractiveCatch"))
+        {
+            _isInVision = false;
+        }
+    }
+
+    private void UpdateDistanceValue(Collider other)
+    {
+        if (_lookObjectContainer.TryGetValue(other.transform, out float value))
+        {
+            _lookObjectContainer[other.transform] = Vector3.Distance(other.transform.position, transform.position);
+        }
+    }
+
+    private Transform GetObject(Dictionary<Transform, float> _objectContainer) => _objectContainer.OrderBy(x => x.Value)
+                                                                                                .ToDictionary(x => x.Key, x => x.Value)
+                                                                                                .First().Key;
+
+    private bool IsObjectNear(Transform catchObject)
+    {
+        float distance = Vector3.Distance(catchObject.transform.position, transform.position);
+
+        if (distance > 0.4f && distance < 1.5f) 
+            return true;
+        else 
+            return false;
+    }
+
+    private void IKActivate()
+    {
+        if (_animator.GetBool("IsStay"))
+        {
+            _isActive = true;
+            _weight = Mathf.Lerp(_weight, 3, Time.deltaTime * _speed);
+        }
+        else
+        {
+            _isActive = false;
+            _weight = 0;
+        }
     }
 }
